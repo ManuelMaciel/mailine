0c5f530d48dbc6a2e99dfa6365a34930a62e6cec
ManuelMaciel <manuelmaciel7001@hotmail.com>
2021-05-31 14:52:21 -0400

    added react navigation an create stack component

---
+++ components/StackNavigator.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+// React Navigation
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator();
+
+
+const StackNavigator = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default StackNavigator
