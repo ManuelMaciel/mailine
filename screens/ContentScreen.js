662b5108a7bed1e3b290ec98e7a8b50efa4f9538
ManuelMaciel <manuelmaciel7001@hotmail.com>
2021-05-31 20:27:12 -0400

    feat: finish content screen

---
+++ screens/ContentScreen.js
@@ -0,0 +1,90 @@
+import React, { useEffect } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  Dimensions,
+  ScrollView,
+  ActivityIndicator,
+  StyleSheet,
+  Image,
+  StatusBar
+} from "react-native"
+import SnackBar from 'react-native-snackbar-component'
+
+const ContentScreen = () => {
+
+  const [ data, setData ] = useState('')
+  const [ loading, setLoading ] = useState(true)
+  const [ emailName, setEmailName ] =  useState(route.params.emailName)
+  const [ emailDomain, setEmailDomain ] = useState(route.params.emailDomain)
+  const [ emailId, setEmailId ] = useState(route.params.emailId)
+  const [ visiblity, setVisiblity] = useState(true)
+
+  useEffect(() => {
+    seeMessageText();
+    return () => {
+      console.log('cleanup')
+    }
+  }, []);
+
+  const seeMessage = () => {
+    // this.props.navigation.navigate("Terms")
+    // this.setState({ visiblity : false })
+  };
+
+  const seeMessageText = () => {
+    fetch("https://www.1secmail.com/api/v1/?action=readMessage&login="+emailName+"&domain="+emailDomain+"&id="+emailId)
+      .then((response) => response.json())
+      .then((json) => {
+        setLoading(true);
+        setData(json["textBody"]);
+        setVisible(false)
+      })
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false))
+  }
+
+  return (
+    <>
+      <StatusBar backgroundColor="#1A1A1F" barStyle="light-content"/>
+        <ScrollView style={styles.container} >
+          {loading ? <ActivityIndicator style={styles.activityIndicatorStyle} color="#FFF" /> : (
+            <View style={styles.textContainer}>
+              <Text selectable={true} style={styles.text}>{data}</Text>
+            </View>
+          )}  
+        </ScrollView>
+        <SnackBar 
+          visible={visiblity} 
+          textMessage="Sólo se mostrará el texto" actionHandler={seeMessage} actionText="Más información"
+          backgroundColor="#121212" messageColor="#FFF" accentColor="#FFF" autoHidingTime={3000}
+        />
+      </>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:"#1A1A1F",
+    height:"90%",
+    width:"100%",
+  },
+  activityIndicatorStyle:{
+    marginTop:"20%"
+  },
+  textContainer:{
+    height:"90%",
+    width:"90%",
+    marginLeft:"8%",
+    marginTop:"10%"
+  },
+  text:{
+    color:"#FFF"
+  }
+})
+
+
+export default ContentScreen
